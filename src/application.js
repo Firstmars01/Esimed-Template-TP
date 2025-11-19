1369ba355e36be515aea8238d63118cbc32135c1
MARIE Clément <clement.marie24@gmail.com>
2025-11-19 14:24:06 +0100

    add scene

--- src/application.js
+++ src/application.js
@@ -2,6 +2,8 @@ import * as THREE from 'three/webgpu'
 import {Scene} from "./scene.js";
 import {Camera} from "./camera.js";
 import {OrbitControls} from "three/examples/jsm/Addons.js";
+import {GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
+
 
 
 export class Application {
@@ -12,21 +14,33 @@ export class Application {
         document.body.appendChild(this.renderer.domElement)
 
         this.scene = new Scene()
-        this.scene.addCube()
+        //this.scene.addCube()
+        this.scene.loadScene('/scenes/scene_1.json')
+
         this.camera = new Camera().camera
         this.scene.addAmbiantLight()
         this.controls = new OrbitControls(this.camera, this.renderer.domElement)
-        this.scene.addSunLight()
+        this.scene.addDirectionalLight()
 
         this.initParams()
-        this.scene.addGround(this.groundTexture[4], this.groundParams.repeats)
+        this.scene.addGround(this.groundTexture[3], this.groundParams.repeats)
 
+      /*
+        const gtlfLoader = new GLTFLoader();
+        gtlfLoader.load('/models/spaceship.glb', (gltf) => {
+          const obj = gltf.scene
+          obj.position.z = 20
+          this.scene.add(obj)
+        });
+*/
         this.renderer.setAnimationLoop(this.render.bind(this))
     }
 
     render() {
         // calculs application
         this.renderer.render(this.scene.scene, this.camera)
+
+
     }
 
 
@@ -34,9 +48,9 @@ export class Application {
       this.groundTexture =
         [
           'aerial_grass_rock',
-          'brown_mud_leaves',
+          'brown_mud_leaves_01',
           'forest_floor',
-          'forest_ground',
+          'forrest_ground_01',
           'gravelly_sand'
         ]
 
--- src/scene.js
+++ src/scene.js
@@ -1,16 +1,18 @@
 import * as THREE from 'three/webgpu'
 import {createStandardMaterial} from "./tools.js";
+import { loadGltf } from './tools.js';
 
 export class Scene {
 
   constructor() {
     this.scene = new THREE.Scene();
 
+    this.loadedModels = {};
   }
 
   addCube() {
     const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0xff0000, flatShading: true });
+    const material = new THREE.MeshPhongMaterial({ color: 0xff0000, flatShading: true });
     const cube = new THREE.Mesh(geometry, material);
     this.scene.add(cube);
   }
@@ -18,11 +20,12 @@ export class Scene {
 
   addAmbiantLight(){
 
-    const ambiantLight = new THREE.AmbientLight(0xffffff, 1.0);
+    const ambiantLight = new THREE.AmbientLight(0xffffff, 0.3)
+
     this.scene.add(ambiantLight);
   }
 
-  addSunLight(){
+  addDirectionalLight(){
 
     const sun = new THREE.DirectionalLight(0xffffff, 2.0)
     sun.position.set(3, 50, 0)
@@ -34,7 +37,7 @@ export class Scene {
   }
 
   addGround(texture, repeats){
-    const geometry = new THREE.PlaneGeometry(2000, 2000)
+    const geometry = new THREE.PlaneGeometry(500, 500)
 
     const material = createStandardMaterial(texture, repeats)
 
@@ -44,5 +47,83 @@ export class Scene {
     //ground.receiveShadow = true
     this.scene.add(ground)
   }
+/*
+  async loadScene(url) {
 
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const nodes = data.nodes || [];
+
+    for (const obj of nodes) {
+
+      const name = obj.name;
+
+      // Charger uniquement si pas déjà chargé
+      if (!this.loadedModels[name]) {
+        this.loadedModels[name] = await loadGltf(name);
+      }
+
+      // loadGltf retourne déjà un OBJET SCENE prêt à cloner
+      const original = this.loadedModels[name];
+      const instance = original.clone(true);
+
+      // Position
+      if (obj.position) {
+        instance.position.fromArray(
+          obj.position.split(',').map(Number)
+        );
+      }
+
+      // Rotation (quaternion)
+      if (obj.rotation) {
+        instance.quaternion.fromArray(
+          obj.rotation.split(',').map(Number)
+        );
+      }
+
+      // Scale
+      if (obj.scale) {
+        instance.scale.fromArray(
+          obj.scale.split(',').map(Number)
+        );
+      }
+
+      this.scene.add(instance);
+    }
+  }
+*/
+
+  async loadScene(url) {
+    const response = await fetch(url)
+    const data = await response.json()
+    for (const obj of data.nodes) {
+      if (this.loadedModels[obj.name] == undefined) {
+        this.loadedModels[obj.name] = await loadGltf(obj.name)
+      }
+      let mesh = this.loadedModels[obj.name].clone()
+      mesh.position.fromArray(obj.position.split(',').map(Number))
+      mesh.quaternion.fromArray(obj.rotation.split(',').map(Number))
+      mesh.scale.fromArray(obj.scale.split(',').map(Number))
+      mesh.traverse(o => {
+        if (o.isMesh) {
+          o.userData = {
+            isSelectable: true,
+            object : mesh,
+          };
+        }});
+      this.scene.add(mesh)
+    }
+    let params = {}
+    if (data.params) {
+      if (data.params.skybox) {
+        params.skybox = data.params.skybox
+      }
+      if (data.params.ground) {
+        params.ground = data.params.ground
+      }
+    }
+    return params
+  }
 }
+
